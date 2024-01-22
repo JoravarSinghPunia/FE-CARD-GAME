16fad851eed2c2102f9026db661cef6d12473bce
JoravarSinghPunia <140854182+JoravarSinghPunia@users.noreply.github.com>
2024-01-22 19:29:48 +0000

    card match game ready for deployment

---
+++ index.js
@@ -0,0 +1,46 @@
+const fruits = [
+  "🥝",
+  "🥝",
+  "🍋",
+  "🍋",
+  "🍉",
+  "🍉",
+  "🍒",
+  "🍒",
+  "🍓",
+  "🍓",
+  "🍑",
+  "🍑",
+];
+const randomFruits = fruits.sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+for (let i = 0; i < randomFruits.length; i++) {
+  let box = document.createElement("div");
+  box.className = "fruits";
+  box.innerHTML = randomFruits[i];
+
+  box.onclick = function () {
+    this.classList.add("boxOpen");
+    setTimeout(function () {
+      if (document.querySelectorAll(".boxOpen").length > 1) {
+        if (
+          document.querySelectorAll(".boxOpen")[0].innerHTML ==
+          document.querySelectorAll(".boxOpen")[1].innerHTML
+        ) {
+          document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
+          document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+
+          if (document.querySelectorAll(".boxMatch").length == fruits.length) {
+            alert("Win");
+          }
+        } else {
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+        }
+      }
+    }, 500);
+  };
+  document.querySelector(".game").appendChild(box);
+}
